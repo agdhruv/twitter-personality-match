f7d0a29c2be3997cfa999e822a2b70f4bc105674
agdhruv <dhruv_agarwal@live.com>
2017-06-09 01:16:12 +0530

    Make API call.

--- app.py
+++ app.py
@@ -14,4 +14,11 @@ twitter_access_token = '1159596978-e9JGcGdlc8kDenvOCmAGZ4pKCQDGUETrkCORCPg'
 twitter_access_secret = tokens.twitter_access_secret
 
 
+# Create instance of Twitter API
+twitter_api = twitter.Api(consumer_key=twitter_consumer_key, consumer_secret=twitter_consumer_secret, access_token_key=twitter_access_token, access_token_secret=twitter_access_secret)
+
+# Start making API call
+handle = "@ATPWorldTour"
+statuses = twitter_api.GetUserTimeline(screen_name=handle, count=200, include_rts=False)
+
 
